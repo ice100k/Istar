699f3fb8a313715611fb6cfa902f8c6e15e793d7
ice100k <61597736+ice100k@users.noreply.github.com>
2020-09-02 11:37:17 +0100

    Added commands (run Admin:Help to see them). output is now labeled

--- Istar/shell.py
+++ Istar/shell.py
@@ -5,19 +5,46 @@
 #<<<<<<<<<<<<<<<<<<IMPORTS>>>>>>>>>>>>>>>>>>
 
 import istar
+from os import system, name
+
+#<<<<<<<<<<<<<<<<FUNCTIONS>>>>>>>>>>>>>>>>>>
+
+def clear():
+
+
+    if name == 'nt':
+        _ = system('cls')
+
+
+    else:
+        _ = system('clear')
 
 #<<<<<<<<<<<<<<<<<RUNTIME>>>>>>>>>>>>>>>>>>>
 
 while True:
 
-	text = input("Istar:  ")
+	text = input(">>>  ")
 
-	if text == "quit()":
+	if text == "Admin:Terminate":
 		quit()
+
+	elif text == "Admin:Prism":
+		print("[Admin] Prism is now admin")
+
+	elif text == "Admin:Clear":
+		clear()
+		print("[Admin] Cleared")
+
+	elif text == "Admin:Help":
+		print("[Admin] Here is the Admin Help list:")
+		print("^C (ctrl + c) overrides everything and terminates the program")
+		print("Admin:Terminate Terminates the program")
+		print("Admin:Help idk why you need to know about this you litteraly need to know it to get here lol")
+		print("Admin:Clear clears everything on screen")
 
 	else:
 
 		result, error = istar.run('<stdin>', text)
 
-		if error: print(error.as_string())
-		elif result: print(result)
+		if error: print("[Istar] " + error.as_string())
+		elif result: print("[Istar] " + str(result))
